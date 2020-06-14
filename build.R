ebf860013bb7f8e9e4269224161e1a6d20867c4f
John Helveston <john.helveston@gmail.com>
2020-06-14 07:24:41 -0400

    p4a: updated icons with custom fix

--- build.R
+++ build.R
@@ -1,2 +1,7 @@
+# https://github.com/rstudio/rmarkdown/issues/1554
+# Install my version of rmarkdown with icon fix
+# Temporary solution until the bug is fixed
+# devtools::install_github('jhelvy/rmarkdown')
+
 # rmarkdown::render_site()
-rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
+# rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
