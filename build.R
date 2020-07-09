8d59317126a7bb1d2e7f989f98d00e733736a867
John Paul Helveston <john.helveston@gmail.com>
2020-07-09 15:14:03 -0400

    fixed slack font awesome icon

--- build.R
+++ build.R
@@ -1,7 +1,9 @@
+# Issue with font awesome icons in the navbar:
 # https://github.com/rstudio/rmarkdown/issues/1554
-# Install my version of rmarkdown with icon fix
-# Temporary solution until the bug is fixed
-# devtools::install_github('jhelvy/rmarkdown')
 
-# rmarkdown::render_site()
-# rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
+# Install my version of rmarkdown with icon fix until the bug is fixed:
+devtools::install_github('jhelvy/rmarkdown')
+
+
+rmarkdown::render_site()
+rmarkdown::render(input = 'README.Rmd', output_format = 'github_document')
