f78cba13e9c1de8603483da2cc68626e501d85c7
John Helveston <john.helveston@gmail.com>
2020-09-01 17:49:41 -0400

    added thumbnail

--- class/1-getting-started/build_pdf.R
+++
@@ -1,6 +0,0 @@
-# Build and save the slides
-lesson <- strsplit(here::here(), '/')[[1]]
-lesson <- paste0('slides-', lesson[length(lesson)])
-pagedown::chrome_print(
-    input  = here::here(paste0(lesson, '.html')),
-    output = here::here(paste0(lesson, '.pdf')))
