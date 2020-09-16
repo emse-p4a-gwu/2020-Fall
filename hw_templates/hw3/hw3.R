2ab0e2bb584baff37141b3544d2a0e64c39a55e9
John Helveston <john.helveston@gmail.com>
2020-09-16 14:58:20 -0400

    fixed turtle test code

---
+++ hw_templates/hw3/hw3.R
@@ -0,0 +1,60 @@
+# Name:      Last, First
+# GW Net ID: Insert your GWNetID here
+
+# I worked with the following classmates on this assignment:
+# 1) Name: Last, First
+# 2) Name: Last, First
+
+######################################################################
+# Place your solutions here
+######################################################################
+
+integerSquareRoot <- function(x) {
+    return(42)
+}
+
+fabricYards <- function(inches) {
+    return(42)
+}
+
+fabricExcess <- function(inches) {
+    return(42)
+}
+
+isPerfectCube <- function(x) {
+    return(42)
+}
+
+kthDigit <- function(x, k) {
+    return(42)
+}
+
+numberOfPoolBalls <- function(rows) {
+    return(42)
+}
+
+#--------------------------------------------------------------
+# ignore_rest: The autograder will ignore all code below here
+
+# Turtle Graphics - no test functions for these, just run them and view the
+# output to make sure it's producing the correct graphic
+
+turtleSquare <- function(s) {
+    return(42)
+}
+
+# Run to test the output
+library(TurtleGraphics)
+turtle_init()
+turtleSquare(50)
+
+# Bonus Credit (Optional):
+
+turtleTriangle <- function(s) {
+    return(42)
+}
+
+# Run to test the output
+library(TurtleGraphics)
+turtle_init()
+turtleTriangle(50)
