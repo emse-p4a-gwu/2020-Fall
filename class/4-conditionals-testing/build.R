4dc1b38db52eb83de33127f51f2c47f25ee43398
John Helveston <john.helveston@gmail.com>
2020-09-21 14:41:30 -0400

    added w4 slides

---
+++ class/4-conditionals-testing/build.R
@@ -0,0 +1,14 @@
+library(magick)
+
+# Build and save the slides
+lesson <- strsplit(here::here(), '/')[[1]]
+lesson <- paste0('slides-', lesson[length(lesson)])
+pagedown::chrome_print(
+    input  = here::here(paste0(lesson, '.html')),
+    output = here::here(paste0(lesson, '.pdf')))
+
+# Create thumbnail image
+pagedown::chrome_print(
+    input  = here::here(paste0(lesson, '.html')),
+    output = here::here('thumbnail.png'), 
+    format = 'png')
