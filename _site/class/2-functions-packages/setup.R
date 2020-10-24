eae103946a85e74c04f7d22645ca464291ca4693
John Helveston <john.helveston@gmail.com>
2020-10-24 08:34:32 -0400

    removed all python evaluated chunks from L9l

---
+++ _site/class/2-functions-packages/setup.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(countdown)
+library(fontawesome)
+library(kableExtra)
+library(hrbrthemes)
+options(
+    htmltools.dir.version = FALSE,
+    knitr.table.format = "html",
+    knitr.kable.NA = '',
+    tibble.width = Inf)
+knitr::opts_chunk$set(
+    warning = FALSE,
+    message = FALSE,
+    fig.retina = 3)
--- foo.py
+++ foo.py
@@ -5,3 +5,11 @@ def isOdd(n):
 
 n1 = isOdd(4)
 n2 = isOdd(3)
+
+class Animal:
+    def __init__(self, species, sound):
+        self.species = species
+        self.sound = sound
+
+    def introduce(self):
+        print("I'm a " + self.species + " and I say " + self.sound)
