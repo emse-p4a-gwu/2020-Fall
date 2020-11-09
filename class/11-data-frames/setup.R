795220f7fdf2614b8f50a18bf85f9983c1abf86c
jhelvy <john.helveston@gmail.com>
2020-11-09 20:55:42 +0000

    Deploying to gh-pages from  @ e958201babd9721436239bc711b167da3f53c505 🚀

---
+++ class/11-data-frames/setup.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(countdown)
+library(fontawesome)
+library(kableExtra)
+options(
+    htmltools.dir.version = FALSE,
+    knitr.table.format = "html",
+    knitr.kable.NA = '',
+    tibble.width = Inf)
+knitr::opts_chunk$set(
+    warning = FALSE,
+    message = FALSE,
+    fig.retina = 3)
+
+beatles_reset <- tibble(
+    firstName   = c("John", "Paul", "Ringo", "George"),
+    lastName    = c("Lennon", "McCartney", "Starr", "Harrison"),
+    instrument  = c("guitar", "bass", "drums", "guitar"),
+    yearOfBirth = c(1940, 1942, 1940, 1943),
+    deceased    = c(TRUE, FALSE, FALSE, TRUE)
+)
+
+pathToData <- here::here('data', 'data.csv')
+wildlife_impacts <- read_csv(pathToData)
